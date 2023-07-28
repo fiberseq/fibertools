d3e47ecf3adce9d4acc056c67d2cd013080569d3
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-28 09:47:23 -0700

    readme

--- fibertools/unionbg.py
+++ fibertools/unionbg.py
@@ -96,8 +96,6 @@ def make_summary_stats(matrix, log_q_values=None, weights=None):
     # adjust for expected amount of coverage
     if True:
         if weights is not None:
-            average_log_q_value = np.nanmean(log_q_vals / cov)
-            logging.info(f"Average log q value: {average_log_q_value}")
             average_log_q_value = np.nanmean(
                 (log_q_vals * weights / weights.sum()) / cov,
                 #weights=weights,
